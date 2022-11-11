7065d000c9a9b3650194cf03af9d8fd8bc9ef5da
Haunzmcmahon <62951967+Haunzmcmahon@users.noreply.github.com>
2022-11-11 16:30:49 +0100

    adapted hop entity with @OneToOne annotation for locationCoordinates

--- springboot/src/main/java/at/fhtw/swen3/persistence/entities/HopEntity.java
+++ springboot/src/main/java/at/fhtw/swen3/persistence/entities/HopEntity.java
@@ -1,4 +1,31 @@
 package at.fhtw.swen3.persistence.entities;
+import lombok.Data;
 
+import javax.persistence.*;
+
+@Entity
+@Data
 public class HopEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    @Column
+    private String hopType;
+
+    @Column
+    private String code;
+
+    @Column
+    private String description;
+
+    @Column
+    private Integer processingDelayMins;
+
+    @Column
+    private String locationName;
+
+    @OneToOne
+    @Column
+    private GeoCoordinateEntity locationCoordinates;
 }
