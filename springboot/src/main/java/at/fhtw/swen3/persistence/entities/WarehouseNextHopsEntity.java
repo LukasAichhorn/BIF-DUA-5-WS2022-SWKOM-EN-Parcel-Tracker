90039d23229545161bcca0b4dfc6046ebd035c07
Haunzmcmahon <62951967+Haunzmcmahon@users.noreply.github.com>
2022-11-11 17:21:13 +0100

    Class variables added to WarehouseEntity and WarehouseNextHopsEntity classes. Unclear db relationships still to do.

--- springboot/src/main/java/at/fhtw/swen3/persistence/entities/WarehouseNextHopsEntity.java
+++ springboot/src/main/java/at/fhtw/swen3/persistence/entities/WarehouseNextHopsEntity.java
@@ -1,4 +1,23 @@
 package at.fhtw.swen3.persistence.entities;
 
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
 public class WarehouseNextHopsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long Id;
+
+    @Column
+    private Integer traveltimeMins;
+
+    /*
+    TODO: db beziehung richtig setzen. (1to1, mto1, etc.)
+    @Column
+    private HopEntity hop;*/
 }
