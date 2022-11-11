1e6577e5b27ae47468f40719bae14e1a0b1cc21e
Haunzmcmahon <62951967+Haunzmcmahon@users.noreply.github.com>
2022-11-11 17:19:24 +0100

    ErrorEntity adapted to use sequentially generated Id. Several Entity classes created.

--- springboot/src/main/java/at/fhtw/swen3/persistence/entities/ErrorEntity.java
+++ springboot/src/main/java/at/fhtw/swen3/persistence/entities/ErrorEntity.java
@@ -2,18 +2,16 @@ package at.fhtw.swen3.persistence.entities;
 
 
 import lombok.Data;
-import org.springframework.context.annotation.Primary;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
 public class ErrorEntity {
 
     @Id
-    long id;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
 
     @Column
     private String errorMessage;
--- springboot/src/main/java/at/fhtw/swen3/persistence/entities/GeoCoordinateEntity.java
+++ springboot/src/main/java/at/fhtw/swen3/persistence/entities/GeoCoordinateEntity.java
@@ -1,4 +1,20 @@
 package at.fhtw.swen3.persistence.entities;
 
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
 public class GeoCoordinateEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    long id;
+
+    @Column
+    private Double lat;
+
+    @Column
+    private Double lon;
 }
