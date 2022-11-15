e44ca92883512c302fc51dfcb555a2078467c712
Haunzmcmahon <62951967+Haunzmcmahon@users.noreply.github.com>
2022-11-15 15:34:13 +0100

    added INSTANCE to all Mapper interfaces

--- springboot/src/main/java/at/fhtw/swen3/services/mapper/WarehouseNextHopsMapper.java
+++ springboot/src/main/java/at/fhtw/swen3/services/mapper/WarehouseNextHopsMapper.java
@@ -3,10 +3,11 @@ package at.fhtw.swen3.services.mapper;
 import at.fhtw.swen3.persistence.entities.WarehouseNextHopsEntity;
 import at.fhtw.swen3.services.dto.WarehouseNextHops;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface WarehouseNextHopsMapper {
-
+    WarehouseNextHopsMapper INSTANCE = Mappers.getMapper(WarehouseNextHopsMapper.class);
     WarehouseNextHops warehouseNextHopsEntitiyToWarehouseNextHopsDto(WarehouseNextHopsEntity warehouseNextHopsEntity);
     WarehouseNextHopsEntity warehouseNextHopsDtoToWarehouseNextHopsEntity(WarehouseNextHops warehouseNextHopsDto);
 
