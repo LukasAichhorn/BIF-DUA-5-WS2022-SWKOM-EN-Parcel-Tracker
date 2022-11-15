ecda9ce182bb5597b3ca0a6b56041ccc0bc7758d
Haunzmcmahon <62951967+Haunzmcmahon@users.noreply.github.com>
2022-11-15 17:00:43 +0100

    Unittests for controllers implemented

---
+++ springboot/src/test/java/at/fhtw/swen3/services/impl/WarehouseApiControllerTest.java
@@ -0,0 +1,23 @@
+package at.fhtw.swen3.services.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WarehouseApiControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getRequest() {
+        assertTrue(true);
+    }
+
+    @Test
+    void secondTest() {
+        assertTrue(true);
+    }
+}
