8e1ad48b377bcfda7cceff1b8ecf469dbe26ce7b
Haunzmcmahon <62951967+Haunzmcmahon@users.noreply.github.com>
2022-11-28 10:42:18 +0100

    ApiController-Testclasses created

---
+++ springboot/src/test/java/at/fhtw/swen3/controller/rest/ParcelApiControllerTest.java
@@ -0,0 +1,37 @@
+package at.fhtw.swen3.controller.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParcelApiControllerTest {
+
+    @Test
+    void reportParcelDelivery() {
+        assertTrue(true);
+    }
+
+    @Test
+    void reportParcelHop() {
+        assertTrue(true);
+
+    }
+
+    @Test
+    void submitParcel() {
+        assertTrue(true);
+
+    }
+
+    @Test
+    void trackParcel() {
+        assertTrue(true);
+
+    }
+
+    @Test
+    void transitionParcel() {
+        assertTrue(true);
+
+    }
+}
---
+++ springboot/src/test/java/at/fhtw/swen3/controller/rest/WarehouseApiControllerTest.java
@@ -0,0 +1,23 @@
+package at.fhtw.swen3.controller.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WarehouseApiControllerTest {
+
+    @Test
+    void exportWarehouses() {
+        assertTrue(true);
+    }
+
+    @Test
+    void getWarehouse() {
+        assertTrue(true);
+    }
+
+    @Test
+    void importWarehouses() {
+        assertTrue(true);
+    }
+}
