83924457c6fe6550719cc4cc9e5d2c108108ee7b
LukasAichhorn <lukas.aichhorn@gmail.com>
2023-01-30 14:42:50 +0100

    adding H2 property file to Tests package

--- springboot/src/test/java/at/fhtw/swen3/openapitools/OpenApiGeneratorApplicationTests.java
+++ springboot/src/test/java/at/fhtw/swen3/openapitools/OpenApiGeneratorApplicationTests.java
@@ -2,8 +2,10 @@ package at.fhtw.swen3.openapitools;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("/application-test.properties")
 class OpenApiGeneratorApplicationTests {
 
     @Test
